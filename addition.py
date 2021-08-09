1a3eb00fb4d99c90efbda6c1babf21d53e48554b
govind <govind@gmail.com>
2021-08-09 23:34:29 +0530

    adding files

---
+++ addition.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the secomd number"))
+
+c = a + b
+
+print ("The sum of {} and {} is {}".format(a,b,c))
+
---
+++ product.py
@@ -0,0 +1,7 @@
+a = int(input("Enter the first number"))
+b = int(input("Enter the second number"))
+
+c = a * b
+
+print ("The product of {} and {} is {}".format(a,b,c))
+
